c0a64f012dda97c287326af38105c60e2eb331e2
Klaus Keppler <kk@keppler-it.de>
2025-08-03 16:38:06 +0200

    added splash image, improved wake-up and power-down, cleaned up

--- main.c
+++ main.c
@@ -8,43 +8,36 @@
 
 #include <avr/pgmspace.h>
 #include <util/delay.h>
+#include "beep.h"
 #include "button.h"
 #include "i2cmaster.h"
 #include "menu.h"
+#include "splash.h"
 #include "timer.h"
 #include "SSD1306.h"
 #include "SCD4x.h"
 #include "VCC.h"
 #include "main.h"
 
-#define VCC_MIN 280 // minimum voltage: 2.80V
-#define VCC_MAX 370 // maximum voltage: 3.70V
+#define VCC_MIN 280 /* minimum voltage: 2.80V */
+#define VCC_MAX 370 /* maximum voltage: 3.70V */
 
-typedef enum {
-    BEEP_SHORT = 1,
-    BEEP_RELAX = 2,
-    BEEP_WARN = 4,
-} beep_t;
-
-#define BEEP_PWM 1
-#undef BEEP_PIEZO
-
-const char app_version[] PROGMEM = "V29 - 2025-07-09";
+const char app_version[] PROGMEM = "V32 - 2025-08-03";
 
 // show battery status
 static uint8_t oldPct = 0;
 static uint8_t vccPct = 0;
 static void writeBattery(uint8_t pct) {
-    if (oldPct == pct) return; // nothing has changed
+    if (oldPct == pct) return; /* nothing has changed */
     oldPct = pct;
-    //uint8_t img[] = {0x18, 0x7e, 0x42, 0x42, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e};
+    /* uint8_t img[] = {0x18, 0x7e, 0x42, 0x42, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e}; */
     uint8_t img[13];
     img[0] = 0x18;
     img[1] = img[12] = 0x7e;
     for (uint8_t x=0; x<10; x++) {
         img[11-x] = pct > x*10 ? 0x7e : 0x42;
     }
-    SSD1306_writeImg(13, 8, img, 0);
+    SSD1306_writeImg(0, 0, 13, 8, img, 0);
     uint8_t x = SSD1306_writeInt(2, 0, pct, 10, 0x00, 0);
     SSD1306_writeChar(x++, 0, '%', 0);
     while (x < 6) SSD1306_writeChar(x++, 0, ' ', 0);
@@ -55,28 +48,12 @@ static uint16_t co2max = 0;
 static uint16_t lastThreshold = 2000;
 static uint8_t belowThresholdSecs = 0;
 
-static void app_beep(const beep_t t) {
-    uint8_t len;
-#if defined(BEEP_PIEZO)
-    PORTB |= (1 << PB3);
-    len = t == BEEP_WARN ? 600 : 200;
-    while (len >= 100) { _delay_ms(100); len -= 100; }
-    PORTB &= ~(1 << PB3);
-#elif defined(BEEP_PWM)
-    TCCR1 = 1<<CS10;
-    GTCCR = 1 << PWM1B | 1 << COM1B1;
-
-    OCR1C = t == BEEP_WARN ? 250 : 220; // 1 MHz / 4 kHz = 250...
-    OCR1B = OCR1C/2; // OCR1C/2 for 50% duty cycle
-    for (len=t; len>0; len--) _delay_ms(100);
-    OCR1C = t == BEEP_WARN ? 220 : 250; // 1 MHz / 4 kHz = 250...
-    OCR1B = OCR1C/2; // OCR1C/2 for 50% duty cycle
-    for (len=t; len>0; len--) _delay_ms(100);
-
-    TCCR1 = 0;
-    GTCCR = 0;
-#endif
-}
+typedef enum {
+    MAIN_STATE_EMPTY,
+    MAIN_STATE_STARTING,
+    MAIN_STATE_RUNNING,
+} main_state_t;
+static main_state_t main_state = MAIN_STATE_EMPTY;
 
 static void main_enter(void) {
     tick = 0;
@@ -89,15 +66,7 @@ static void main_enter(void) {
     oldPct = 0xff; // force update
     writeBattery(vccPct);
 
-    SSD1306_writeString(4, 3, PSTR("."), 1);
-    SSD1306_writeString(7, 2, PSTR("^C"), 1);
-    SSD1306_writeString(14, 2, PSTR("%"), 1);
-    SSD1306_writeString(14, 3, PSTR("RH"), 1);
-
-    SSD1306_writeString(0, 5, PSTR("CO2 MAX:"), 1);
-    SSD1306_writeInt(9, 5, co2max, 10, 0, 0);
-    SSD1306_writeString(12, 6, PSTR("CO2"), 1);
-    SSD1306_writeString(12, 7, PSTR("PPM"), 1);
+    main_state = MAIN_STATE_EMPTY;
 }
 
 static void main_leave(void) {
@@ -105,7 +74,7 @@ static void main_leave(void) {
 }
 
 static void main_loop(void) {
-    static const char tickChars[] = {'[','\\',']','\\'};
+    static const char tickChars[] = {'<','=','>','='};
     static uint64_t old_ms = 0;
     uint8_t err;
 
@@ -116,17 +85,46 @@ static void main_loop(void) {
     }
 
     if (timer_millis() - old_ms >= 1000) {
+        if (main_state == MAIN_STATE_STARTING) {
+            SSD1306_writeInt(6, 5, 90 - (timer_millis() / 1000), 10, 0, 2);
+        }
         err = SCD4x_getData();
         if (err > 1) {
             SSD1306_writeString(0, 3, PSTR("ERR:       "), 1);
             SSD1306_writeInt(5, 3, err, 16, 0x00, 0);
         } else if (err == 0) {
-            if (SCD4x_VALUE_co2 > co2max) co2max = SCD4x_VALUE_co2;
-            SSD1306_writeInt(9, 5, co2max, 10, 0, 0);
-            SSD1306_writeInt(1, 6, SCD4x_VALUE_co2, 10, 0x04, 5);
-            SSD1306_writeInt(0, 2, SCD4x_VALUE_temp / 10, 10, 0x04, 2);
-            SSD1306_writeInt(5, 2, SCD4x_VALUE_temp % 10, 10, 0x04, 0);
-            SSD1306_writeInt(10, 2, SCD4x_VALUE_humidity, 10, 0x04, 2);
+            if (main_state == MAIN_STATE_EMPTY) {
+                SSD1306_writeString(4, 3, PSTR("."), 1);
+                SSD1306_writeString(7, 2, PSTR("[C"), 1);   /* '[' is displayed as '°' */
+                SSD1306_writeString(14, 2, PSTR("%"), 1);
+                SSD1306_writeString(14, 3, PSTR("RH"), 1);
+
+                if (main_state == MAIN_STATE_RUNNING || timer_millis() > 90000) {
+                    SSD1306_writeString(0, 5, PSTR("CO2 MAX:"), 1);
+                    SSD1306_writeInt(9, 5, co2max, 10, 0, 0);
+                } else {
+                    SSD1306_writeString(0, 5, PSTR("INIT:"), 1);
+                    SSD1306_writeInt(6, 5, 90 - (timer_millis() / 1000), 10, 0, 2);
+                }
+                SSD1306_writeString(12, 6, PSTR("CO2"), 1);
+                SSD1306_writeString(12, 7, PSTR("PPM"), 1);
+                main_state = MAIN_STATE_STARTING;
+            }
+            if (main_state == MAIN_STATE_RUNNING || timer_millis() > 90000) {
+                if (main_state == MAIN_STATE_STARTING) SSD1306_writeString(0, 5, PSTR("CO2 MAX:"), 1);
+                main_state = MAIN_STATE_RUNNING;
+            }
+
+            if (main_state == MAIN_STATE_RUNNING) {
+                if (SCD4x_VALUE_co2 > co2max) co2max = SCD4x_VALUE_co2;
+                SSD1306_writeInt(9, 5, co2max, 10, 0, 0);
+            }
+            uint8_t flags = SSD1306_FLAG_DOUBLE;
+            if (main_state == MAIN_STATE_STARTING) flags |= SSD1306_FLAG_LIGHT;
+            SSD1306_writeInt(1, 6, SCD4x_VALUE_co2, 10, flags, 5);
+            SSD1306_writeInt(0, 2, SCD4x_VALUE_temp / 10, 10, flags, 2);
+            SSD1306_writeInt(5, 2, SCD4x_VALUE_temp % 10, 10, flags, 0);
+            SSD1306_writeInt(10, 2, SCD4x_VALUE_humidity, 10, flags, 2);
 
             /* check threshold */
             if (SCD4x_VALUE_co2 > lastThreshold + 2000) {
@@ -137,7 +135,7 @@ static void main_loop(void) {
                 if (SCD4x_VALUE_co2 >= 20000) cnt++;
                 if (SCD4x_VALUE_co2 >= 24000) cnt++;
                 while (cnt-- > 0) {
-                    app_beep(BEEP_WARN);
+                    beep(BEEP_WARN);
                     _delay_ms(200);
                 }
 
@@ -149,7 +147,7 @@ static void main_loop(void) {
                     if (belowThresholdSecs == 0) {
                         /* if less than last threshold for more than 60sec, reduce threshold by 2000ppm */
                         if (lastThreshold >= 6000) lastThreshold -= 2000;
-                        app_beep(BEEP_RELAX);
+                        beep(BEEP_RELAX);
                     }
                 }
             } else {
@@ -158,9 +156,8 @@ static void main_loop(void) {
             }
         }
         if (tick % 10 == 0) {
-            // update VCC display every ~10 seconds
+            /* update VCC display every ~10 seconds */
             uint16_t vcc = VCC_get();
-            // SSD1306_writeInt(8, 0, vcc, 10, 0x00, 0); // DEBUG output raw voltage
             if (vcc < VCC_MIN) vcc = VCC_MIN;
             else if (vcc > VCC_MAX) vcc = VCC_MAX;
             vccPct = ((vcc - VCC_MIN) * 100) / (VCC_MAX - VCC_MIN);
@@ -186,66 +183,66 @@ void app_wakeup(uint8_t initial) {
     SSD1306_init();
     SSD1306_clear();
     SSD1306_on();
-    SSD1306_writeString(0, 0, app_version, 1);
 
-    // beep.
-    app_beep(BEEP_RELAX);
+    SSD1306_writeImg(5, 1, splash_width, splash_height, splash_data, 2);
+    SSD1306_writeString(0, 5, app_version, SSD1306_FLAG_PGM);
+
+    /* beep. */
+    beep(BEEP_STARTUP);
 
     if ((err = SCD4x_stopPeriodicMeasurement()) != 0) {
-        SSD1306_writeInt(14, 1, err, 16, 0x00, 0);
+        SSD1306_writeInt(14, 0, err, 16, 0x00, 0);
     }
 
     /* detect sensor type */
     scd4x_sensor_type_t sensorType = SCD4x_getSensorType();
-    SSD1306_writeString(0, 1, PSTR("SENSOR:"), 1);
     if (sensorType == SCD4x_SENSOR_SCD40) {
-        SSD1306_writeString(8, 1, PSTR("SCD40"), 1);
+        SSD1306_writeString(0, 6, PSTR("SCD40"), 1);
     } else if (sensorType == SCD4x_SENSOR_SCD41) {
-        SSD1306_writeString(8, 1, PSTR("SCD41"), 1);
+        SSD1306_writeString(0, 6, PSTR("SCD41"), 1);
     } else if (sensorType == SCD4x_SENSOR_ERROR) {
-        SSD1306_writeString(8, 1, PSTR("ERROR"), 1);
+        SSD1306_writeString(0, 6, PSTR("ERROR"), 1);
         while(1);
     } else {
-        SSD1306_writeString(8, 1, PSTR("UNKNOWN"), 1);
+        SSD1306_writeString(0, 6, PSTR("UNKNW"), 1);
         while(1);
+    }
+
+    {
+        SSD1306_writeString(7, 6, PSTR("VCC 0.00V"), 1);
+        uint16_t vcc = VCC_get();
+        uint8_t x;
+        x = SSD1306_writeInt(11, 6, vcc / 100, 10, 0x00, 0);
+        //SSD1306_writeChar(x++, 5, '.', 0x00);
+        x = SSD1306_writeInt(++x, 6, vcc % 100, 10, SSD1306_FLAG_FILL_ZERO, 2);
+        //SSD1306_writeChar(x++, 5, 'V', 0x00);
     }
 
     /* read serial number */
     if (initial) {
         uint8_t serial[6];
-        SSD1306_writeString(0, 2, PSTR("SERIAL:"), 1);
         if ((err = SCD4x_getSerialNumber(serial)) != 0) {
             /* error while reading serial */
-            SSD1306_writeString(0, 3, PSTR("CRC ERROR"), 1);
+            SSD1306_writeString(0, 7, PSTR("CRC ERROR"), 1);
         } else {
             uint8_t i;
             for (i=0; i<3; i++) {
                 uint8_t u = serial[i*2];
                 if (u < 0x10) {
-                    SSD1306_writeInt(i*5, 3, 0, 16, 0x00, 0);
-                    SSD1306_writeInt((i*5)+1, 3, u, 16, 0x00, 0);
+                    SSD1306_writeInt(i*5, 7, 0, 16, 0x00, 0);
+                    SSD1306_writeInt((i*5)+1, 7, u, 16, 0x00, 0);
                 } else {
-                    SSD1306_writeInt(i*5, 3, u, 16, 0x00, 0);
+                    SSD1306_writeInt(i*5, 7, u, 16, 0x00, 0);
                 }
                 u = serial[(i*2)+1];
                 if (u < 0x10) {
-                    SSD1306_writeInt((i*5)+2, 3, 0, 16, 0x00, 0);
-                    SSD1306_writeInt((i*5)+3, 3, u, 16, 0x00, 0);
+                    SSD1306_writeInt((i*5)+2, 7, 0, 16, 0x00, 0);
+                    SSD1306_writeInt((i*5)+3, 7, u, 16, 0x00, 0);
                 } else {
-                    SSD1306_writeInt((i*5)+2, 3, u, 16, 0x00, 0);
+                    SSD1306_writeInt((i*5)+2, 7, u, 16, 0x00, 0);
                 }
             }
         }
-    }
-
-    {
-        SSD1306_writeString(0, 5, PSTR("VCC: 0.00 V"), 1);
-        uint16_t vcc = VCC_get();
-        uint8_t x;
-        x = SSD1306_writeInt(5, 5, vcc / 100, 10, 0x00, 0);
-        //SSD1306_writeChar(x++, 5, '.', 0x00);
-        x = SSD1306_writeInt(++x, 5, vcc % 100, 10, SSD1306_FLAG_FILL_ZERO, 2);
-        //SSD1306_writeChar(x++, 5, 'V', 0x00);
     }
 
     /* reset max and threshold values after power-off */
@@ -262,22 +259,12 @@ int main(void) {
 
     button_init();
 
-    // init buzzer
-#if defined(BEEP_PIEZO)
-    // PB3: piezo
-    DDRB |= (1 << DDB3);
-    PORTB &= ~(1 << PB3);
-    // PB4: unused PWM buzzer
-    DDRB &= ~(1 << DDB4);   // set port mode to INPUT
-    PORTB |= (1 << PB4);    // enable pull-up
-#elif defined(BEEP_PWM)
+    /* init sound transducer */
+    beep_init();
+
     // PB3: unused piezo
     DDRB &= ~(1 << DDB3);   // set port mode to INPUT
     PORTB |= (1 << PB3);    // enable pull-up
-    // PB4: PWM with OC1B pin (PB4)
-    DDRB |= (1 << DDB4);
-    PORTB &= ~(1 << PB4);
-#endif
 
     /* initialize I²C bus */
     i2c_init();
